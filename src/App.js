dba710ce9fdbce2f6df85b7d989b76f7df3a3927
unknown <rnrudxo2872@naver.com>
2021-11-08 17:10:16 +0900

    styled-components 테스트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import styled from "styled-components"
+
+const Father = styled.div`
+  display:flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width:100px;
+  height:100px;
+`
+const Circle = styled(Box)`
+  border-radius:100%;
+`
+
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <Father className="App">
+      <Box bgColor={"tomato"}/>
+      <Box bgColor={"blue"}/>
+      <Circle bgColor="red"/>
+    </Father>
   );
 }
 
