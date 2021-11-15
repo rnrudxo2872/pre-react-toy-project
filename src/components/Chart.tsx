4fb19037251699e480955bdbff42afdf984d5615
unknown <rnrudxo2872@naver.com>
2021-11-16 01:10:58 +0900

    chart 수정

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -15,7 +15,12 @@ function Chart({coinId}:ChartProps) {
               id: "basic-bar"
             },
             xaxis: {
-              categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+              labels:{
+                show:false
+              }
+            },
+            yaxis: {
+              show:false
             },
             theme: {
                 mode:"dark"
@@ -23,8 +28,8 @@ function Chart({coinId}:ChartProps) {
         }
         const series = [
             {
-              name: "series-1",
-              data: [30, 40, 45, 50, 49, 60, 70, 91]
+              name: "Price",
+              data: data?.map(val => val.close)
             }
           ]
         
