1c75fc0769eb6a50d5df7257df0a6f81582fb1a3
unknown <rnrudxo2872@naver.com>
2021-12-09 11:25:16 +0900

    이전 버튼 링크 수정

--- src/components/PrevButton.tsx
+++ src/components/PrevButton.tsx
@@ -2,11 +2,9 @@ import { useHistory } from "react-router";
 import { PreBtn } from "../styledComponets/PrevButton.styled";
 
 function PrevButton() {
-    const history = useHistory();
-    const onClick = () => history.goBack();
-    return (
-        <PreBtn onClick={onClick}>‹</PreBtn>
-    )
+  const history = useHistory();
+  const onClick = () => history.push("/");
+  return <PreBtn onClick={onClick}>‹</PreBtn>;
 }
 
 export default PrevButton;
